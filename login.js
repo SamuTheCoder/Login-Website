9a47130ae2c000a1141cf50722469a64915b4630
Samuel Teixeira <steixeira@ua.pt>
2022-08-31 12:21:26 +0100

    First Commit, website files added

---
+++ login.js
@@ -0,0 +1,7 @@
+function validate(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    if(email = "steixeira@ua.pt" && password == "admin") alert("Logged In Successfuly!");
+    else alert("Something went wrong");
+}
