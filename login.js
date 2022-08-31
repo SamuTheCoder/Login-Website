fa662ffbb9df8da8b4af0acf60862d1d93bfba4f
Samuel Teixeira <steixeira@ua.pt>
2022-08-31 13:16:40 +0100

    Few changes on the js

--- login.js
+++ login.js
@@ -1,7 +1,13 @@
 function validate(){
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-    if(email = "steixeira@ua.pt" && password == "admin") alert("Logged In Successfuly!");
-    else alert("Something went wrong");
+    if(email = "steixeira@ua.pt" && password == "admin") {
+        alert("Logged In Successfuly!");
+        return false;
+    }
+    else {  
+        alert("Something went wrong");
+        return false;
+    }
 }
